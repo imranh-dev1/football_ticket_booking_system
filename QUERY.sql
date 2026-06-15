67402f7b41a22838f0db1a058f9425f4fa7fa65c
imran hossain <imranme.global@gmail.com>
2026-06-15 12:47:13 +0600

    users data insert

--- QUERY.sql
+++ QUERY.sql
@@ -67,3 +67,20 @@ create table bookings (
         ) 
     )
 );
+
+
+-- USERS DATA INSERT
+
+insert into users (
+    user_id, 
+    full_name, 
+    email, 
+    role, 
+    phone_number
+) 
+values
+  
+(1, 'Tanvir Rahman', 'tanvir@mail.com', 'Football Fan', '+8801711111111'),
+(2, 'Asif Haque', 'asif@mail.com', 'Football Fan', '+8801722222222'),
+(3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'),
+(4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL);
